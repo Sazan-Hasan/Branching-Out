4797a0edac7e904f7c8ca34a3d5ab2abb1e48dd3
Sazan Hasan <Sazan.HasanHaji@gmail.com>
2026-02-27 13:05:22 +0100

    add filtering for email

--- filter_users.py
+++ filter_users.py
@@ -20,9 +20,16 @@ def filter_users_by_age(age):
         print(user)
 
 
+def filter_users_by_email(email):
+    users = load_users()
+    filtered_users = [user for user in users if user["email"].lower() == email.lower()]
+    for user in filtered_users:
+        print(user)
+
+
 if __name__ == "__main__":
     filter_option = input(
-        "What would you like to filter by? ('name' or 'age'): "
+        "What would you like to filter by? ('name' or 'age' or 'email'): "
     ).strip().lower()
 
     if filter_option == "name":
@@ -32,6 +39,10 @@ if __name__ == "__main__":
     elif filter_option == "age":
         age_to_search = int(input("Enter an age to filter users: ").strip())
         filter_users_by_age(age_to_search)
+
+    elif filter_option == "email":
+        email_to_search = input("Enter an email to filter users: ").strip()
+        filter_users_by_email(email_to_search)
 
     else:
         print("Filtering is not yet supported.")
